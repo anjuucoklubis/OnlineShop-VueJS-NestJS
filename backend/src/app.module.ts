fc82aa0d2f2d4996b38e9579a55805ecd4e65508
Anju Lubis <78084093+anjuucoklubis@users.noreply.github.com>
2024-01-20 22:42:12 +0700

    +product

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoryProductController } from './back-dashboard/category-product/category-product.controller';
 import { CategoryProductModule } from './back-dashboard/category-product/category-product.module';
-import { PrismaService } from './prisma/prisma.service';
-import { CategoryProductService } from './back-dashboard/category-product/category-product.service';
 import { PrismaModule } from './prisma/prisma.module';
-
+import { ProductModule } from './back-dashboard/product/product.module';
 
 @Module({
-  imports: [PrismaModule,CategoryProductModule],
+  imports: [PrismaModule,CategoryProductModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
